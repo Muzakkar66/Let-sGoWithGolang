0d6d958e50a58df2f7c902d87fb6eec830e6fa68
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2022-12-28 14:00:59 +0500

    added switch and case statment

--- 13switchCase/main.go
+++ 13switchCase/main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println("Welcome in switch and case in golang")
 	rand.Seed(time.Now().Unix())
 	diceNumber := rand.Intn(6) + 1 //it will generate btween 0 to 5 rang and then adding 1 into genrated number from 0 to 5
-	fmt.Println("Value of dice is: ", diceNumber)
+	fmt.Println("Value of dice isuuuuu: ", diceNumber)
 
 	// switch and case statment implementing
 	//agar diceNumber 3 generate kary to 3 wala case chal jay
@@ -21,8 +21,10 @@ func main() {
 		fmt.Println("Dice value is 2 ")
 	case 3:
 		fmt.Println("Dice value is 3")
+		fallthrough // mean jab case 3 y to sath 4 ko b chla day
 	case 4:
 		fmt.Println("Dice value is 4")
+		fallthrough //issi tara jab 4 chaly to 5 ko b chala dy
 	case 5:
 		fmt.Println("Dice value is 5")
 	case 6:
